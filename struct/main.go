786e3fedc78bb92779525d7fde1f82b77a6ba6fa
aqibcs <aqibshahcs@gmail.com>
2023-12-27 15:28:46 +0500

    Struct

---
+++ struct/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"time"
+)
+
+type User struct {
+	FirstName string
+	LastName string
+	PhoneNumber string
+	Age int
+	BirthDate time.Time
+}
+
+func main() {
+	user := User {
+		FirstName: "Aqib",
+		LastName: "Shah",
+		PhoneNumber: "0090909009",
+	}
+
+	log.Println(user.FirstName, user.LastName, "Birthdate:", user.BirthDate)
+}
