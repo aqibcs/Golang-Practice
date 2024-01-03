e110e8f3dd690913224daf31021e825995d8b138
aqibcs <aqibshahcs@gmail.com>
2024-01-03 11:25:22 +0500

    Data Structure

--- data-structure/main.go
+++ data-structure/main.go
@@ -1,47 +1,69 @@
 package main
 
 import (
-	"log"
-	"sort"
+	"container/list"
+	"fmt"
 )
 
-type User struct {
-	FirstName string
-	LastName  string
+// LRUCache struct represents the least recently used chache.
+type LRUCache struct {
+	capacity int                      // maximum capacity of the cache
+	cache    map[string]*list.Element // map to store chache entries for quick lookup.
+	lruList  *list.List               // doubly linked list to maintain the order of recently used entries.
+}
+
+// entry struct represents a key-value piar in the cache.
+type entry struct {
+	key   string // key of the entry.
+	value int    // value associated with the key.
+}
+
+// Get retrieves the value associatedwith the specified key from the cache
+func (lru *LRUCache) Get(key string) int {
+	if elem, ok := lru.cache[key]; ok {
+		lru.lruList.MoveToFront(elem)
+		return elem.Value.(*entry).value
+	}
+	return -1 // key not found in the cache.
+}
+
+// Put inserts a key-value piar into the chache.
+// if the cache is at its capacity, it evicts the Least recently used entry
+func (lru *LRUCache) Put(key string, value int) {
+	if elem, ok := lru.cache[key]; ok {
+		// update existing entry
+		elem.Value.(*entry).value = value
+		lru.lruList.MoveToFront(elem)
+	} else {
+		// add new entry
+		if len(lru.cache) >= lru.capacity {
+			// Evict the Least recently used entry
+			back := lru.lruList.Back()
+			if back != nil {
+				delete(lru.cache, back.Value.(*entry).key)
+				lru.lruList.Remove(back) // Remove the least recently used entry from the list.
+			}
+		}
+		newEntry := &entry{key, value}
+		newElem := lru.lruList.PushFront(newEntry)
+		lru.cache[key] = newElem // update the map with the new entry
+	}
+}
+
+func newLRU(size int) *LRUCache {
+	return &LRUCache{
+		capacity: size,
+		cache:    make(map[string]*list.Element),
+		lruList:  list.New(),
+	}
 }
 
 func main() {
-	// Maps
-	myMap := make(map[string]User)
-
-	me := User{
-		FirstName: "Aqib",
-		LastName:  "Shah",
-	}
-
-	myMap["me"] = me
-
-	log.Println(myMap["me"].FirstName)
-
-	// Slices
-	var mySlice []int
-
-	mySlice = append(mySlice, 2)
-	mySlice = append(mySlice, 1)
-	mySlice = append(mySlice, 3)
-
-	sort.Ints(mySlice)
-
-	log.Println(mySlice)
-
-	// Shorthand for slices
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	log.Println(numbers)
-
-	// Print first two elements of slices, starting at first position
-	log.Println(numbers[0:2])
-
-	// Create a slice of strings
-	names := []string{"one", "seven", "fish", "cat"}
-	log.Println(names)
+	x := newLRU(2)
+	x.Put("1", 1)           // 1
+	x.Put("2", 2)           // 1, 2
+	fmt.Println(x.Get("1")) // ok
+	x.Put("3", 3)           // 1, 3
+	fmt.Println(x.Get("2")) // not ok
+	fmt.Println(x.Get("3")) // ok
 }
